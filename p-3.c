c239caa3231d57dcb3bcd1ca08669ce25c33ddf5
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-03-21 11:45:09 +0600

    03

---
+++ p-3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,row,col;
+printf("Enter N=");
+scanf("%d",&n);
+for(row=1;row<=n;row++)
+{
+    for(col=1;col<=row;col++){
+
+        printf("%d",col%2);
+
+    }
+    printf("\n");
+}
+  return 0 ;
+}
