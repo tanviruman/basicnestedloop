8479425702026bb40cf381ee1000639fad3368ed
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-03-21 21:47:44 +0600

    new

---
+++ Pattern type-3/p-1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int n,row,col;
+printf("Enter N=");
+scanf("%d",&n);
+for(row=1;row<=n-1;row++)
+{
+    for(col=1;col<=row;col++){
+
+        printf("*");
+
+    }
+    printf("\n");
+}
+for(row=n;row>=1;row--)
+{
+    for(col=1;col<=row;col++){
+
+        printf("*");
+
+    }
+    printf("\n");
+}
+  return 0 ;
+}
