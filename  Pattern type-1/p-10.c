5779d8299de8a406bbc973215b9fc2f0f802c376
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-03-21 16:55:09 +0600

    added

---
+++  Pattern type-1/p-10.c
@@ -0,0 +1,18 @@
+//right tri #
+#include<stdio.h>
+int main()
+{
+int n,row,col;
+printf("Enter N=");
+scanf("%d",&n);
+for(row=1;row<=n;row++)
+{
+    for(col=1;col<=row;col++){
+
+        printf("#");
+
+    }
+    printf("\n");
+}
+  return 0 ;
+}
